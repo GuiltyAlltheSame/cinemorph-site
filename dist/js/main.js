f1a5107ddb2d637280716eb0052200936704f865
GuiltyAlltheSame <vegas281@gmail.com>
2025-11-07 23:56:28 -0800

    +prim menu +adj glow +js for menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,19 @@
-console.log("cinemorph ready");
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.getElementById('vhsMenu');
+  const btnClose = document.getElementById('vhsMenuClose');
+  const hotspot = document.querySelector('.hotspot-vhs');
+
+  // Открытие меню
+  hotspot.addEventListener('click', (e) => {
+    e.preventDefault(); // если это <a>, не переходит по ссылке
+    menu.classList.add('open');
+  });
+
+  // Закрытие меню
+  btnClose.addEventListener('click', () => {
+    menu.classList.remove('open');
+  });
+});
