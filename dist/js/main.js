c061b5f4624c45869e61e3f37a01aba919980a37
GuiltyAlltheSame <117597178+GuiltyAlltheSame@users.noreply.github.com>
2026-05-14 20:54:27 -0700

    second atempt reload project

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menu = document.getElementById('vhsMenu');
-  const btnClose = document.getElementById('vhsMenuClose');
-  const hotspot = document.querySelector('.hotspot-vhs');
-
-  // Переключение меню по клику на коробку
-  hotspot.addEventListener('click', (e) => {
-    e.preventDefault();
-    menu.classList.toggle('open');
-  });
-
-  // Принудительное закрытие по клику на кнопку
-  btnClose.addEventListener('click', (e) => {
-    e.stopPropagation(); // предотвращает "всплытие" и повторный триггер
-    menu.classList.remove('open');
-  });
-});
+console.log("cinemorph ready");
